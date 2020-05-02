b66a75512e689350ac70ee74d79d3d40564ec928
louishhy <61550059+louishhy@users.noreply.github.com>
2020-05-03 00:45:46 +0800

    Add time calculating functions and leaderboards

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include<vector>
 #include<string>
 #include<map>
+#include<ctime>
+#include<iostream>
+#include<fstream>
+#include<string>
 using namespace std;
 #define N 105
 
@@ -12,21 +16,30 @@ bool mine_board[N][N];
 char game_board[N][N];
 int M,Num;
 
-void menu()//初始界面 done
+void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag );
+void read_leaderboard();
+void write_leaderboard(string user_name, double time_record);
+
+void menu()//menu done
 {
 	char tmp_diff[20];
 	puts("-----------------Minesweeper-----------------");
 	puts("Difficulty:");
 	puts("Easy: Board_width=15 Mine_number=30");
 	puts("Difficult: Board_width=30 Mine_number=400");
+	puts("Leaderboards");
 	puts("---------------------------------------------");
-	printf("Please enter the difficulty of your choice(Easy/Difficult): ");
+	printf("Please enter your choice(Easy/Difficult/Leaderboards): ");
 	scanf("%s",&tmp_diff);
 	if(tmp_diff[0]=='E') M=15,Num=30;
 	if(tmp_diff[0]=='D') M=30,Num=400;
+	if(tmp_diff[0]=='L'){
+		read_leaderboard();
+		menu();
+	}
 	return;
 }
-void initialize()//初始化 done
+void initialize()//initialization done
 {
 	memset(mine_board,sizeof(mine_board),0);
 	for(int i=1;i<N;++i)
@@ -45,7 +58,7 @@ void display_mine_board()
 	}
 	return;
 }
-void display_game_board()//显示棋盘 done
+void display_game_board()//game board display done
 {
 	printf("   ");
 	for(int i=1;i<=M;++i) 
@@ -63,7 +76,7 @@ void display_game_board()//显示棋盘 done
 	}
 	return;	
 }
-void generate(int mine_num)//随机生成雷 done
+void generate(int mine_num)//random mine generation done
 {
 	srand(time(NULL));
 	while(mine_num)
@@ -81,22 +94,23 @@ void generate(int mine_num)//随机生成雷 done
 
 int sweep(int x,int y)
 {
-	if(mine_board[x][y]==1)//踩中雷，失败 
+	if(mine_board[x][y]==1)//Touch the mine, failed
 	{
-		puts("--------------------Faild--------------------");
-		display_mine_board();//显示正确位置 
+		puts("--------------------Failed--------------------");
+		display_mine_board();//display the correct mine board using 0-1
 		puts("---------------------------------------------");
 		return 0;
 	}
-	if(mine_board[x][y]==0)//没踩中，递归更新game_board 
+	if(mine_board[x][y]==0)//refresh game board recursively 
 	{
 		return 1;
 	}
 }
-void Player_operation()
+void Player_operation(clock_t& start_time, clock_t& end_time, double& time_record)
 {
 	menu();
 	generate(Num);
+	time_calculation(start_time, end_time, time_record, 0);
 	while(Num)
 	{
 		display_game_board();
@@ -106,12 +120,61 @@ void Player_operation()
 		result=sweep(tmp_x,tmp_y);
 		if(!result) break;
 	}
+	
+	time_calculation(start_time, end_time, time_record, 1);
+	
+	string name;
+	printf("Please enter your name: ");
+	cin >> name;
+	write_leaderboard(name, time_record);
+	return;
+}
+
+void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag ){        //A time calculation function, flag 0 -> start, flag 1 -> end and print time used
+	if (flag==0){
+		start = clock();
+	}
+	else if (flag == 1) {
+		end = clock();
+		cout << "Time used: " << (end - start) / (double)CLOCKS_PER_SEC << " seconds." <<endl;
+		time_interval = (end - start) / (double)CLOCKS_PER_SEC;
+	}
+}
+
+void write_leaderboard(string user_name, double time_record){            //input the user name and the time record in seconds, then write to leaderboards
+	ofstream fout;
+	fout.open("leaderboard.txt",ios::app);
+	if(fout.fail()){
+		cout << "Error in opening the leaderboard!"<<endl;
+		return;
+	}
+	fout << user_name << " " << time_record << "seconds" <<endl;
+	fout.close();
+	return;
+}
+
+void read_leaderboard(){    //displays the leaderboard
+	cout << "--------------------LEADERBOARD--------------------" <<endl;
+	ifstream fin;
+	fin.open("leaderboard.txt",ios::app);
+	if(fin.fail()){
+		cout << "Error in opening the leaderboard!"<<endl;
+		return;
+	}
+	string line;
+	while(getline(fin,line)){
+		cout << line << endl;
+	}
+	cout<<endl;
+	fin.close();
 	return;
 }
 
 int main()
 {
 	initialize();
-	Player_operation();
+	clock_t starting_time, ending_time;
+	double time_record;
+	Player_operation(starting_time, ending_time, time_record);
 	return 0;
 }
