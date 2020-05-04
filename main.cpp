c068feed4db5f5fed7a66459ea582c0dad4161e9
zwderek <61866929+zwderek@users.noreply.github.com>
2020-05-04 17:38:40 +0800

    Update main.cpp
    Complete DFS update and victory judgment
    Perfect details

--- main.cpp
+++ main.cpp
@@ -16,30 +16,121 @@ bool mine_board[N][N];
 char game_board[N][N];
 int M,Num;
 
-void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag );
-void read_leaderboard();
-void write_leaderboard(string user_name, double time_record);
+void menu();//Print menu
+void initialize();//Initialize each array
+void Player_operation(clock_t& start_time, clock_t& end_time, double& time_record);
+void display_mine_board();
+void display_game_board();
+void generate(int mine_num);//Random mine generation
+int count_mine(int x,int y);//Count the number of surrounding mines
+void sweep_dfs(int x,int y,bool vis[N][N]);//Update game board via DFS
+int sweep(int x,int y);
+void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag ); //A time calculation function, flag 0 -> start, flag 1 -> end and print time used
+void read_leaderboard();//displays the leaderboard
+void write_leaderboard(string user_name, double time_record);//input the user name and the time record in seconds, then write to leaderboards
 
-void menu()//menu done
+int main()
+{
+	initialize();
+	clock_t starting_time, ending_time;
+	double time_record;
+	Player_operation(starting_time, ending_time, time_record);
+	return 0;
+}
+
+void Player_operation(clock_t& start_time, clock_t& end_time, double& time_record)
+{
+	menu();
+	generate(Num);//Generate a mine board witn Num mines
+	time_calculation(start_time, end_time, time_record, 0);
+	display_game_board();
+	while(Num)//Still have mines
+	{
+		int tmp_x,tmp_y,result;
+		char mode[5];
+		printf("Please enter the coordinate: ");
+		scanf("%s%d%d",&mode,&tmp_x,&tmp_y);
+		if(tmp_x<1||tmp_x>M||tmp_y<1||tmp_y>M)//Coordinate exceeds limits
+		{
+			puts("Illegal coordinate, please enter again.");
+			continue;
+		}
+		if(mode[0]=='S')
+		{
+			result=sweep(tmp_x,tmp_y);
+			if(result==0)//Coordinate exceeds limits
+			{
+				puts("Illegal coordinate, please enter again.");
+				continue;
+			}
+			if(result==1) break;//Stepped on a mine
+		}		
+		if(mode[0]=='M')
+		{
+			if(game_board[tmp_x][tmp_y]=='*')
+			{
+				game_board[tmp_x][tmp_y]='?';
+				if(mine_board[tmp_x][tmp_y]) Num--;//Successfully find the mine location
+			}
+			else 
+			{
+				puts("Illegal coordinate, please enter again.");
+				continue;
+			}
+		}
+		display_game_board();
+	}
+	
+	if(!Num) 
+	{
+		puts("---------------------WIN!--------------------");
+		display_mine_board();
+	}
+	
+	time_calculation(start_time, end_time, time_record, 1);
+	
+	string name;
+	printf("Please enter your name: ");
+	cin >> name;
+	write_leaderboard(name, time_record);
+	return;
+}
+
+void menu()
 {
 	char tmp_diff[20];
 	puts("-----------------Minesweeper-----------------");
-	puts("Difficulty:");
-	puts("Easy: Board_width=15 Mine_number=30");
-	puts("Difficult: Board_width=30 Mine_number=400");
-	puts("Leaderboards");
+	puts("Mode selection:");
+	puts("	Beginner: Board_width=5 Mine_number=3");
+	puts("	Easy: Board_width=9 Mine_number=10");
+	puts("	Normal: Board_width=15 Mine_number=30");
+	puts("	Hard: Board_width=30 Mine_number=200");
+	puts("	Leaderboards");
 	puts("---------------------------------------------");
-	printf("Please enter your choice(Easy/Difficult/Leaderboards): ");
+	puts("Operation:");
+	puts("	S x y: Sweep and view the status of coordinates(x,y)");
+	puts("	M x y: Mark the location with a question mark '?' (May be a mine at this location)");
+	puts("Conditions for players to win:");
+	puts("	All mines are marked with '?'");
+	puts("---------------------------------------------");
+	printf("Please enter your choice(Easy/Normal/Hard/Leaderboards): ");
 	scanf("%s",&tmp_diff);
-	if(tmp_diff[0]=='E') M=15,Num=30;
-	if(tmp_diff[0]=='D') M=30,Num=400;
-	if(tmp_diff[0]=='L'){
+	if(tmp_diff[0]=='B') M=5,Num=3;
+	else if(tmp_diff[0]=='E') M=9,Num=10;
+ 	else if(tmp_diff[0]=='N') M=15,Num=30;
+	else if(tmp_diff[0]=='H') M=30,Num=200;
+	else if(tmp_diff[0]=='L'){
 		read_leaderboard();
+		menu();
+	}
+	else {
+		puts("Illegal input, please enter again.");
 		menu();
 	}
 	return;
 }
-void initialize()//initialization done
+
+void initialize()
 {
 	memset(mine_board,sizeof(mine_board),0);
 	for(int i=1;i<N;++i)
@@ -49,6 +140,7 @@ void initialize()//initialization done
 	}
 	return;
 }
+
 void display_mine_board()
 {
 	for(int i=1;i<=M;++i)
@@ -58,7 +150,8 @@ void display_mine_board()
 	}
 	return;
 }
-void display_game_board()//game board display done
+
+void display_game_board()
 {
 	printf("   ");
 	for(int i=1;i<=M;++i) 
@@ -76,7 +169,8 @@ void display_game_board()//game board display done
 	}
 	return;	
 }
-void generate(int mine_num)//random mine generation done
+
+void generate(int mine_num)
 {
 	srand(time(NULL));
 	while(mine_num)
@@ -88,49 +182,73 @@ void generate(int mine_num)//random mine generation done
 			--mine_num;
 		}
 	}
-	display_mine_board();
+//	display_mine_board();
+	return;
+}
+
+int count_mine(int x,int y)
+{
+	int tmp_num=0;
+	if(mine_board[x-1][y-1]) tmp_num++;
+	if(mine_board[x][y-1]) tmp_num++;
+	if(mine_board[x+1][y-1]) tmp_num++;
+	if(mine_board[x-1][y]) tmp_num++;
+	if(mine_board[x+1][y]) tmp_num++;
+	if(mine_board[x-1][y+1]) tmp_num++;
+	if(mine_board[x][y+1]) tmp_num++;
+	if(mine_board[x+1][y+1]) tmp_num++;
+	return tmp_num;
+}
+
+void sweep_dfs(int x,int y,bool **vis)
+{
+	if(x<1||x>M||y<1||y>M) return;
+	if(mine_board[x][y]==1) return;
+	if(vis[x][y]) return;
+	vis[x][y]=1;
+	if(count_mine(x,y))
+	{
+		game_board[x][y]='0'+count_mine(x,y);
+		return;
+	}
+	game_board[x][y]='\0';
+	sweep_dfs(x-1,y-1,vis);sweep_dfs(x,y-1,vis);sweep_dfs(x+1,y-1,vis);sweep_dfs(x-1,y,vis);
+	sweep_dfs(x+1,y,vis);sweep_dfs(x-1,y+1,vis);sweep_dfs(x,y+1,vis);sweep_dfs(x+1,y+1,vis);
 	return;
 }
 
 int sweep(int x,int y)
 {
+	if(x<1||x>M||y<1||y>M)
+	{
+		return 0;
+	}
 	if(mine_board[x][y]==1)//Touch the mine, failed
 	{
 		puts("--------------------Failed--------------------");
 		display_mine_board();//display the correct mine board using 0-1
 		puts("---------------------------------------------");
-		return 0;
+		return 1;
 	}
 	if(mine_board[x][y]==0)//refresh game board recursively 
 	{
-		return 1;
+		bool **vis=new bool* [N];
+		for(int i=0;i<N;++i)
+		{
+			vis[i]=new bool[N];
+			for(int j=0;j<N;++j) vis[i][j]=0;
+		}
+		sweep_dfs(x,y,vis);
+		for(int i=0;i<N;++i)
+		{
+			delete[] vis[i];
+		}
+		delete[] vis;
+		return 2;
 	}
-}
-void Player_operation(clock_t& start_time, clock_t& end_time, double& time_record)
-{
-	menu();
-	generate(Num);
-	time_calculation(start_time, end_time, time_record, 0);
-	while(Num)
-	{
-		display_game_board();
-		printf("Please enter the coordinate: ");
-		int tmp_x,tmp_y,result;
-		scanf("%d%d",&tmp_x,&tmp_y);
-		result=sweep(tmp_x,tmp_y);
-		if(!result) break;
-	}
-	
-	time_calculation(start_time, end_time, time_record, 1);
-	
-	string name;
-	printf("Please enter your name: ");
-	cin >> name;
-	write_leaderboard(name, time_record);
-	return;
 }
 
-void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag ){        //A time calculation function, flag 0 -> start, flag 1 -> end and print time used
+void time_calculation(clock_t& start, clock_t& end, double& time_interval, int flag ){       
 	if (flag==0){
 		start = clock();
 	}
@@ -141,7 +259,7 @@ void time_calculation(clock_t& start, clock_t& end, double& time_interval, int f
 	}
 }
 
-void write_leaderboard(string user_name, double time_record){            //input the user name and the time record in seconds, then write to leaderboards
+void write_leaderboard(string user_name, double time_record){            
 	ofstream fout;
 	fout.open("leaderboard.txt",ios::app);
 	if(fout.fail()){
@@ -153,7 +271,7 @@ void write_leaderboard(string user_name, double time_record){            //input
 	return;
 }
 
-void read_leaderboard(){    //displays the leaderboard
+void read_leaderboard(){    
 	cout << "--------------------LEADERBOARD--------------------" <<endl;
 	ifstream fin;
 	fin.open("leaderboard.txt",ios::app);
@@ -168,13 +286,4 @@ void read_leaderboard(){    //displays the leaderboard
 	cout<<endl;
 	fin.close();
 	return;
-}
-
-int main()
-{
-	initialize();
-	clock_t starting_time, ending_time;
-	double time_record;
-	Player_operation(starting_time, ending_time, time_record);
-	return 0;
 }
